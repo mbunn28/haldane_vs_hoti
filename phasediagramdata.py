eb2ca40ce002042f9433c53f2c494c157e62d0fc
Matt Bunney <matt_bunney@live.com.au>
2020-07-10 18:09:43 +1000

    Parameter restriction = Ben is happy

--- phasediagramdata.py
+++ phasediagramdata.py
@@ -12,47 +12,56 @@ Corners = False,
 alpha = 0,
 hal = 0,
 M=0,
-N=25)
+N=14)
 
-t=150
-gap = np.zeros((2*t+1,2*t+1))
+t=90
+gap = np.zeros((2*t+1,t+1))
 hal_val = np.zeros(2*t+1)
-alph_val = np.zeros(2*t+1)
+alph_val = np.zeros(t+1)
+lattice.large_alpha = True
+
 for k in range(0,t):
+    
     lattice.hal = k/t
     hal_val[k] = lattice.hal
     hal_val[2*t-k] = 2-lattice.hal
 
-    for m in range(0,t):
+    for m in range(0,t+1):
 
-        lattice.alpha = m/t
-        alph_val[m] = lattice.alpha
-        alph_val[2*t-m] = 2-lattice.alpha
+        if m == t:
+            lattice.alpha = 1
+        else:
+            lattice.alpha = m/t
+            alph_val[t-m] = 2-lattice.alpha
 
-        lattice.large_hal= False
-        lattice.large_alpha = False
+        # lattice.large_hal= False
+        # lattice.large_alpha = False
 
-        lattice.initialize_hamiltonian()
-        lattice.eigensystem()
-        gap[k,m] = np.min(np.abs(lattice.energies))
+        # lattice.initialize_hamiltonian()
+        # lattice.eigensystem()
+        # gap[k,m] = np.min(np.abs(lattice.energies))
 
         lattice.large_hal = True
 
-        lattice.initialize_hamiltonian()
-        lattice.eigensystem()
-        gap[2*t-k,m] = np.min(np.abs(lattice.energies))
+        # lattice.initialize_hamiltonian()
+        # lattice.eigensystem()
+        # gap[2*t-k,m] = np.min(np.abs(lattice.energies))
 
-        lattice.large_alpha = True
-
-        lattice.initialize_hamiltonian()
-        lattice.eigensystem()
-        gap[2*t-k,2*t-m] = np.min(np.abs(lattice.energies))
+        if (lattice.hal<0.1) or ((lattice.alpha >= -(5/18)*(lattice.hal-1)+0.2) and (lattice.alpha <= -(5/9)*(lattice.hal-1)+0.5)):
+            lattice.initialize_hamiltonian()
+            lattice.eigensystem()
+            gap[2*t-k,t-m] = np.min(np.abs(lattice.energies))
+        else:
+            gap[2*t-k,t-m] = np.NaN
 
         lattice.large_hal = False
 
-        lattice.initialize_hamiltonian()
-        lattice.eigensystem()
-        gap[k,2*t-m] = np.min(np.abs(lattice.energies))
+        if (lattice.alpha >= 0.5*lattice.hal - 0.3) and (lattice.alpha <= (2/3)*(lattice.hal-0.25)):
+            lattice.initialize_hamiltonian()
+            lattice.eigensystem()
+            gap[k,t-m] = np.min(np.abs(lattice.energies))
+        else:
+            gap[k,t-m] = np.NaN
 
 lattice.large_hal= False
 lattice.hal=1
@@ -60,25 +69,29 @@ hal_val[t]=1
 
 for m in range(0,t):
 
-    lattice.large_alpha = False 
+    # lattice.large_alpha = False 
     lattice.alpha = m/t
 
-    lattice.initialize_hamiltonian()
-    lattice.eigensystem()
-    gap[t,m] = np.min(np.abs(lattice.energies))
+    # lattice.initialize_hamiltonian()
+    # lattice.eigensystem()
+    # gap[t,m] = np.min(np.abs(lattice.energies))
 
-    lattice.large_alpha = True
+    # lattice.large_alpha = True
 
-    lattice.initialize_hamiltonian()
-    lattice.eigensystem()
-    gap[t,2*t-m] = np.min(np.abs(lattice.energies))
+    if (lattice.alpha <= 0.5) and (lattice.alpha >= 0.2):
+        lattice.initialize_hamiltonian()
+        lattice.eigensystem()
+        gap[t,t-m] = np.min(np.abs(lattice.energies))
+    else:
+        gap[t,t-m] = np.NaN
 
 lattice.hal=1
 lattice.alpha=1
-alph_val[t]=1
-lattice.initialize_hamiltonian()
-lattice.eigensystem()
-gap[t,t] = np.min(np.abs(lattice.energies))
+alph_val[0]=1
+# lattice.initialize_hamiltonian()
+# lattice.eigensystem()
+# gap[t,0] = np.min(np.abs(lattice.energies))
+gap[t,0]=np.NaN
 
 path = "output/phasediagram"
 if not os.path.exists(path):
@@ -87,3 +100,6 @@ if not os.path.exists(path):
 joblib.dump(gap, f"{path}/N{lattice.N}_gap")
 joblib.dump(hal_val, f"{path}/N{lattice.N}_hal_val")
 joblib.dump(alph_val, f"{path}/N{lattice.N}_alph_val")
+# print(gap)
+# print(hal_val)
+# print(alph_val)
