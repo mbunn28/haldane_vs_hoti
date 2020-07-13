e4c58de7273eb5c948f9674fd79559403b56ea8b
Matt Bunney <matt_bunney@live.com.au>
2020-07-13 22:11:10 +1000

    Split up intervals, ready to run at uni, plot well

--- phasediagramdata.py
+++ phasediagramdata.py
@@ -12,80 +12,62 @@ Corners = False,
 alpha = 0,
 hal = 0,
 M=0,
-N=16)
+N=4)
 
-t=150
-gap = np.zeros((2*t+1,t+1))
-hal_val = np.zeros(2*t+1)
+t=100 #multiples of twenty only
+delta = 1/t
 alph_val = np.zeros(t+1)
 lattice.large_alpha = True
 
-for k in range(0,t):
-    
-    lattice.hal = k/t
-    hal_val[k] = lattice.hal
-    hal_val[2*t-k] = 2-lattice.hal
+V = 0
+Vmin = [0, 0.1, 0.55, 0.75, 0.25]
+Vmax = [0.1, 0.55, 1.0, 1.0, 0.75]
+v_vals = np.arange(Vmin[V],Vmax[V],delta)
+VLarge = ['True', 'True', 'True', 'False', 'False']
 
+gap = np.zeros((v_vals.size,t+1))
+hal_val = np.zeros(v_vals.size)
+
+for k in range(0, v_vals.size):
+    
+    lattice.hal = v_vals[k]
+    lattice.large_hal = VLarge[V]
+    if lattice.large_hal == 'False':
+        hal_val[k] = lattice.hal
+    elif lattice.large_hal == 'True':
+        hal_val[v_vals.size-k-1] = 2-lattice.hal
+  
     for m in range(0,t+1):
 
-        print(f"{k*(t+1)+m}/{t*(t+1)}", end='\r')
+        print(f"{k*(t+1)+m}/{v_vals.size*(t+1)}", end='\r')
+
         if m == t:
             lattice.alpha = 1
         else:
             lattice.alpha = m/t
             alph_val[t-m] = 2-lattice.alpha
 
-        # lattice.large_hal= False
-        # lattice.large_alpha = False
-        # gap[k,m] = lattice.min_energy()
+        if lattice.large_hal == 'True':
+            if (lattice.hal<0.1) or ((lattice.alpha >= -(1/6)*(lattice.hal-1)+0.3) and (lattice.alpha <= -(1/6)*(lattice.hal-1)+0.5)):
+                gap[v_vals.size-k-1,t-m] = lattice.min_energy()
+            else:
+                gap[v_vals.size-k-1,t-m] = np.NaN
 
-        lattice.large_hal = True
-        # gap[2*t-k,m] = lattice.min_energy()
-
-        if (lattice.hal<0.1) or ((lattice.alpha >= -(1/6)*(lattice.hal-1)+0.3) and (lattice.alpha <= -(1/9)*(lattice.hal-1)+0.5)):
-            gap[2*t-k,t-m] = lattice.min_energy()
-        else:
-            gap[2*t-k,t-m] = np.NaN
-
-        lattice.large_hal = False
-
-        if (lattice.hal >= 0.6) and (lattice.alpha >= 0.75*(lattice.hal-1) + 0.3) and (lattice.alpha <= 0.75*(lattice.hal-1)+0.6):
-            gap[k,t-m] = lattice.min_energy()
-        elif (0.25 <= lattice.hal < 0.6) and (lattice.alpha <= (6/7)*(lattice.hal-0.25)):
-            gap[k,t-m] = lattice.min_energy()
-        else:
-            gap[k,t-m] = np.NaN
-
-lattice.large_hal= False
-lattice.hal=1
-hal_val[t]=1
-
-for m in range(0,t):
-
-    # lattice.large_alpha = False 
-    lattice.alpha = m/t
-    # gap[t,m] = lattice.min_energy()
-
-    # lattice.large_alpha = True
-
-    if (lattice.alpha <= 0.5) and (lattice.alpha >= 0.2):
-        gap[t,t-m] = lattice.min_energy()
-    else:
-        gap[t,t-m] = np.NaN
-
-lattice.hal=1
-lattice.alpha=1
-alph_val[0]=1
-# gap[t,0] = lattice.min_energy()
-gap[t,0]=np.NaN
+        elif lattice.large_hal == 'False':
+            if (lattice.hal >= 0.6) and (lattice.alpha >= 0.75*(lattice.hal-1) + 0.3) and (lattice.alpha <= 0.75*(lattice.hal-1)+0.5):
+                gap[k,t-m] = lattice.min_energy()
+            elif (0.25 <= lattice.hal < 0.6) and (lattice.alpha <= (6/7)*(lattice.hal-0.25)):
+                gap[k,t-m] = lattice.min_energy()
+            else:
+                gap[k,t-m] = np.NaN
 
 path = "output/phasediagram"
 if not os.path.exists(path):
             os.makedirs(path)
 
-joblib.dump(gap, f"{path}/N{lattice.N}_gap")
-joblib.dump(hal_val, f"{path}/N{lattice.N}_hal_val")
-joblib.dump(alph_val, f"{path}/N{lattice.N}_alph_val")
+joblib.dump(gap, f"{path}/N{lattice.N}_gap_v{V}")
+joblib.dump(hal_val, f"{path}/N{lattice.N}_hal_val_v{V}")
+joblib.dump(alph_val, f"{path}/N{lattice.N}_alph_val_v{V}")
 # print(gap)
 # print(hal_val)
 # print(alph_val)
--- plotphasediagram.py
+++ plotphasediagram.py
@@ -5,14 +5,42 @@ import numpy as np
 import joblib
 import os
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
-N = 14
+N = 16
 path = "output/phasediagram"
 gap = joblib.load(f"{path}/N{N}_gap")
 hal_val = joblib.load(f"{path}/N{N}_hal_val")
 alph_val = joblib.load(f"{path}/N{N}_alph_val")
 
-# gap[gap>0.01]= np.NaN
-fig = plt.figure()
-plt.pcolormesh(hal_val, alph_val, np.transpose(gap))
+gap[gap>0.05]= np.NaN
+fig, ax = plt.subplots()
+plt.pcolormesh(hal_val, alph_val, np.transpose(gap), norm = colors.LogNorm(), cmap='inferno')
+cbar = plt.colorbar()
+cbar.ax.get_yaxis().labelpad = 15
+cbar.ax.set_title('Energy Gap')
+plt.title(r'Log Scaled Phase Boundary: N=16, $\Delta$ =8.3e-3')
+
+labels1 =[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+locs1 =[2.0, 1.8, 1.6, 1.4, 1.2, 1.0]
+ax.set_yticklabels(labels1)
+ax.set_yticks(locs1)
+ax.set_ylabel(r'$\beta$')
+
+labels2 =[0.0, 0.5, 1.0]
+locs2 =[2.0, 1.5, 1.0]
+ax.set_xticklabels(labels2)
+ax.set_xticks(locs2)
+ax.set_xlabel('t')
+
+ax2 = ax.twiny() 
+# labels = [w.get_text() for w in ax2.get_xticklabels()]
+# locs=list(ax2.get_xticks())
+labels=[0.0, 0.5, 1.0]
+locs=[0.0, 0.25, 0.5]
+ax2.set_xticklabels(labels)
+ax2.set_xticks(locs)
+ax2.set_xlabel(r'$\lambda$')
+
+plt.gcf().subplots_adjust(top=0.85)
 fig.savefig(f"{path}/N{N}_diagram.pdf")
