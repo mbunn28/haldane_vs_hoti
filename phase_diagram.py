38bcad56d9e0e2f86d29c701d9f03c722fba4a96
Matt Bunney <matt_bunney@live.com.au>
2021-05-13 08:08:39 +1000

    real space plot work

--- phase_diagram.py
+++ phase_diagram.py
@@ -12,28 +12,20 @@ PBC_i = False,
 PBC_j = False,
 Corners = False,
 a = 1,
-b = 0.2,
+b = 0.5,
 t = 1,
-l = 0.05,
+l = 0.2,
 M=0,
 N=20)
 
-lattice.hexamercorners = True
-lattice.fivesites = False
-# folder = "data/11052020/PBC/t1.9_a1.9_N12"
-
-# lattice.energies = joblib.load( f"{folder}_energies")
-# lattice.waves = joblib.load(f"{folder}_waves")
-# t_vals = [0.1,0.2,0.3]
-# for i in range(len(t_vals)):
-# [_,_,p,q] = lattice.make_names()
-
-# print(f"a = {p}, l = {q}")
-# print(f"PBC_i = {lattice.PBC_i}")
-# print(f"PBC_j = {lattice.PBC_j}")
-# print(f"Corners = {lattice.Corners}")
+lattice.cornertype = 'Hexamer'
+lattice.colourcode = True
+lattice.corner_p = 0.8
+lattice.edge_p = 0.98
 
 lattice.single_state()
+
+
 # lattice.energy_plot(r=0.5)
 # lattice.a = t_vals[i]
 # lattice.energy_spectrum('t',t=200,min_val=0.2,max_val=0.6)
